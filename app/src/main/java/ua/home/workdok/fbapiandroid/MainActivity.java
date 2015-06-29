67c382da89ceac54242934d2420facc079ac7dfb
Danil_Kolodiy <WorkDok@WorkDok-PC>
2015-06-29 15:13:04 +0300

    add sdk Facebook

--- app/src/main/java/ua/home/workdok/fbapiandroid/MainActivity.java
+++ app/src/main/java/ua/home/workdok/fbapiandroid/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -34,5 +36,19 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
     }
 }
